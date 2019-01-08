9d90ef43241294a73dc853ef99bc9dc4e2ed7a5e
Pete Young <pete.young@sympatico.ca>
2019-01-08 15:16:43 -0500

    add to  goal list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // intro to javascript
-// alert("Hello World!");
-alert(2 + 2);
-alert(4 * "2");
-alert(8 + "2");
+
+$(document).ready(function() {
+  // alert($("#css-item").text());
+    // alert("jQuery is working");
+    $("#goal-button").click(function() {
+      $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+      $("#new-goal").val("");
+    })  
+  })
